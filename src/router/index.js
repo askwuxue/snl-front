fc1762da38ae9f87df4fdf0dcb4a35bfac0e1ece
askwuxue <972523419@qq.com>
2021-08-03 22:11:04 +0800

    router init

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+// 必须使用这种方式注册VueRouter
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
