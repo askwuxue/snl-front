cf1fc85c4c2d0a422257e0cbffae3c575fc01e62
askwuxue <972523419@qq.com>
2021-08-04 22:15:47 +0800

    add annotation

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import store from './store'
 import ElementUI from 'element-ui'
 
 // 引入主题文件
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
 
 // 引入公共样式
 import '../src/styles/index.scss'
