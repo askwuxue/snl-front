5177d0afe0579a1a91f915c53ffe4fb4dcd7996c
askwuxue <972523419@qq.com>
2021-08-07 11:54:57 +0800

    create login page layout

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,73 @@
 <template>
-  <div>登录</div>
+  <div class="login">
+    <el-form :label-position="labelPosition" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="手机号" prop="phone">
+        <el-input v-model="ruleForm.phone"></el-input>
+      </el-form-item>
+        <el-form-item label="密码" prop="password">
+        <el-input v-model="ruleForm.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Login',
+
+  data () {
+    return {
+      labelPosition: 'top',
+      ruleForm: {
+        phone: '',
+        password: ''
+      },
+      rules: {
+        phone: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { min: 3, max: 5, message: '请输入正确的手机号', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 5, message: '密码不正确', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          console.log('valid: ', valid)
+        }
+      })
+    }
+  }
+}
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.login {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.el-form {
+  padding: 20px;
+  width: 300px;
+  background-color: #fff;
+  border-radius: 10px;
+}
+.el-button {
+  width: 100%;
+}
 
+// TODO elementUI的labelPosition属性值为TOP时，没有左对齐
+// .el-form--label-top .el-form-item__label {
+//   float: left !important;
+// }
 </style>
