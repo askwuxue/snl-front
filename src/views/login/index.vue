90f50b8db2a07a99d44576c5bf182468d36e2f25
askwuxue <972523419@qq.com>
2021-08-09 22:23:08 +0800

    add login function

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,49 +1,99 @@
 <template>
   <div class="login">
-    <el-form :label-position="labelPosition" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-      <el-form-item label="手机号" prop="phone">
+    <el-form
+    :label-position="labelPosition"
+    :model="ruleForm"
+    :rules="rules"
+    ref="ruleForm"
+    label-width="100px"
+    class="demo-ruleForm">
+      <el-form-item
+      label="手机号"
+      prop="phone">
         <el-input v-model="ruleForm.phone"></el-input>
       </el-form-item>
-        <el-form-item label="密码" prop="password">
-        <el-input v-model="ruleForm.password"></el-input>
+        <el-form-item
+        label="密码"
+        prop="password">
+        <el-input
+        type="password"
+        v-model="ruleForm.password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+        <el-button
+        type="primary"
+        :loading="isLoding"
+        @click="submitForm('ruleForm')">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+import { login } from '@/interface/user'
 export default {
   name: 'Login',
 
   data () {
     return {
+      // 登录样式排版
       labelPosition: 'top',
+      // 是否加载中
+      isLoding: false,
       ruleForm: {
-        phone: '',
-        password: ''
+        phone: '17201234567',
+        password: '111111'
       },
       rules: {
         phone: [
           { required: true, message: '请输入手机号', trigger: 'blur' },
-          { min: 3, max: 5, message: '请输入正确的手机号', trigger: 'blur' }
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入正确的手机号', trigger: 'blur' }
         ],
         password: [
           { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 3, max: 5, message: '密码不正确', trigger: 'blur' }
+          { min: 6, max: 18, message: '密码不正确', trigger: 'blur' }
         ]
       }
     }
   },
   methods: {
-    submitForm (formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          console.log('valid: ', valid)
+    // 表单提交验证
+    async submitForm (formName) {
+      // 错误处理
+      try {
+        // 验证成功的处理
+        await this.$refs[formName].validate()
+        // 开启button加载中
+        this.isLoding = true
+        // 请求数据的获取
+        const { data } = await login(this.ruleForm)
+        // 登录成功
+        if (data.state === 1) {
+          this.$message({
+            message: '恭喜你，登录成功',
+            type: 'success',
+            onClose: () => {
+              this.isLoding = true
+            }
+          })
+          this.$router.push({
+            name: 'home'
+          })
+          // 登录失败
+        } else {
+          this.$message.error({
+            message: data.message,
+            showClose: true,
+            // message关闭时回调
+            onClose: () => {
+              this.isLoding = false
+            }
+          })
         }
-      })
+        // 验证失败
+      } catch (error) {
+        console.log('验证失败: ', error)
+      }
     }
   }
 }
