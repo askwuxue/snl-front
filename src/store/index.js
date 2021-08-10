d2a6fdd56f4a8f25c4565b3906785eac68124833
askwuxue <972523419@qq.com>
2021-08-10 23:16:41 +0800

    user login info save

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 登录成功后保存用户信息
+    user: JSON.parse(window.localStorage.getItem('user')) || null
   },
+  // 处理state的同步请求并处理state
   mutations: {
+    setUser (state, payload) {
+      state.user = payload
+      // 登录信息持久化处理
+      window.localStorage.setItem('user', payload)
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  // 接收异步请求，调用mutations
+  actions: {},
+  modules: {}
 })
