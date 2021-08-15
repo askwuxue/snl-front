01ebf06961db572489b104e26a5bd4f83974961c
askwuxue <972523419@qq.com>
2021-08-15 09:57:37 +0800

    append getUserInfo function

--- src/interface/user.js
+++ src/interface/user.js
@@ -1,5 +1,6 @@
 import request from '@/utils/request'
 import qs from 'qs'
+import store from '@/store'
 
 // 用户登录接口功能
 export const login = data => {
@@ -8,5 +9,16 @@ export const login = data => {
     url: '/front/user/login',
     // 对数据进行application/x-www-form-urlencoded处理，qs.stringify对数据进行处理外，默认添加content-type
     data: qs.stringify(data)
+  })
+}
+
+// 获取用户信息
+export const getUserInfo = () => {
+  return request({
+    method: 'GET',
+    url: '/front/user/getInfo',
+    headers: {
+      Authorization: JSON.parse(store.state.user.content).access_token
+    }
   })
 }
