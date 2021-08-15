0841b9cdf5ed67582dea9bbb4a1efc5a23e7b018
askwuxue <972523419@qq.com>
2021-08-15 09:59:30 +0800

    append LoadingUserInfo logic

--- src/views/layout/component/AppHeader.vue
+++ src/views/layout/component/AppHeader.vue
@@ -14,11 +14,11 @@
       <!-- 用户头像 -->
       <el-avatar
       :size="size"
-      src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar>
+      :src="userInfo.portrait || 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'"></el-avatar>
       <i class="el-icon-arrow-down el-icon--right"></i>
     </span>
     <el-dropdown-menu slot="dropdown">
-      <el-dropdown-item>用户信息</el-dropdown-item>
+      <el-dropdown-item>{{userInfo.userName}}</el-dropdown-item>
       <el-dropdown-item divided>退出</el-dropdown-item>
     </el-dropdown-menu>
 </el-dropdown>
@@ -26,8 +26,30 @@
 </template>
 
 <script>
+import { getUserInfo } from '@/interface/user'
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  data () {
+    return {
+      userInfo: null
+    }
+  },
+  // 生命周期函数中不处理具体的业务逻辑
+  created () {
+    this.loadingUserInfo()
+  },
+  methods: {
+    async loadingUserInfo () {
+      // 错误处理
+      try {
+        // 获取数据成功
+        const { data } = await getUserInfo()
+        this.userInfo = data.content
+      } catch (err) {
+        console.log('res: ', err)
+      }
+    }
+  }
 }
 </script>
 
