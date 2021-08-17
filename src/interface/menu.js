ef5bd4875f59b19a5a429fdce9d6a4a948e7b0ca
askwuxue <972523419@qq.com>
2021-08-17 22:52:52 +0800

    create menu interface function

---
+++ src/interface/menu.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+
+// 获取编辑菜单页面信息
+export const getEditMenuInfo = (id = -1) => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getEditMenuInfo',
+    params: {
+      id: id
+    }
+  })
+}
