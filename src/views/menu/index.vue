58ffd942a21d7588159e7803f71b8bbecaef02a7
askwuxue <972523419@qq.com>
2021-08-17 22:55:10 +0800

    update page style and renser

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>首页</div>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+          <el-button
+          @click="handleCreate">添加菜单</el-button>
+      </div>
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容 ' + o }}
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    // 跳转跳转到create-menu
+    handleCreate () {
+      this.$router.push({
+        name: 'menu-create'
+      })
+    }
+  }
+}
 </script>
 
 <style lang="sass" scoped>
