7b455ad53f018d680022b18d2a03d0e553a7730c
askwuxue <972523419@qq.com>
2021-08-19 23:18:48 +0800

    append formatDate filter and create resource request interface

--- src/main.js
+++ src/main.js
@@ -15,6 +15,21 @@ import '../src/styles/index.scss'
 // 将ElementUI注册为Vue插件
 Vue.use(ElementUI)
 
+// 日期格式化过滤器
+Vue.filter('formatDate', function (value) {
+  if (!value) {
+    return 'xxxx-xx-xx xx:xx:xx'
+  }
+  const date = new Date(value)
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDay()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+})
+
 Vue.config.productionTip = false
 
 new Vue({
