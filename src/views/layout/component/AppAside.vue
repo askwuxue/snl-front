f4224560596a024134776a1d4b45a74e0b1b1374
askwuxue <972523419@qq.com>
2021-09-20 21:15:07 +0800

    appside link to index update

--- src/views/layout/component/AppAside.vue
+++ src/views/layout/component/AppAside.vue
@@ -14,15 +14,15 @@
           <i class="el-icon-location"></i>
           <span>权限管理</span>
         </template>
-          <el-menu-item index="role">角色管理</el-menu-item>
-          <el-menu-item index="menu">菜单列表</el-menu-item>
-          <el-menu-item index="resource">资源列表</el-menu-item>
+          <el-menu-item index="/role">角色管理</el-menu-item>
+          <el-menu-item index="/menu">菜单列表</el-menu-item>
+          <el-menu-item index="/resource">资源列表</el-menu-item>
       </el-submenu>
-      <el-menu-item index="course">
+      <el-menu-item index="/course">
         <i class="el-icon-menu"></i>
         <span slot="title">课程管理</span>
       </el-menu-item>
-      <el-menu-item index="user">
+      <el-menu-item index="/user">
         <i class="el-icon-document"></i>
         <span slot="title">用户管理</span>
       </el-menu-item>
@@ -31,10 +31,10 @@
           <i class="el-icon-setting"></i>
           <span>广告管理</span>
         </template>
-          <el-menu-item index="advert">
+          <el-menu-item index="/advert">
              <span>广告列表</span>
           </el-menu-item>
-          <el-menu-item index="advert-space">
+          <el-menu-item index="/advert-space">
              <span>广告位列表</span>
           </el-menu-item>
       </el-submenu>
