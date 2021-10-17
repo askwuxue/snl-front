ab4dc06db7db6244007167b7dee01355ea5f9c61
askwuxue <972523419@qq.com>
2021-10-17 22:58:25 +0800

    update submit methods

--- src/views/role/components/List.vue
+++ src/views/role/components/List.vue
@@ -3,7 +3,7 @@
     <div slot="header" class="clearfix">
       <el-form ref="form" :model="form" label-width="80px">
         <el-form-item label="角色名称">
-          <el-input v-model="form.name"></el-input>
+          <el-input v-model="form.name" @keyup.enter="onSubmit"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">查询</el-button>
